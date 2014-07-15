c0a72097ac0cec56f9951b7ba6153f5253768f4f
git-justinpodur <git-justin@podur.org>
2014-07-15 15:52:33 -0400

    creating user model, controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715185754) do
+ActiveRecord::Schema.define(version: 20140715194515) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20140715185754) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deadline"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
