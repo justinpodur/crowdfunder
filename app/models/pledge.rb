315594cb2b717b01bf3421d24604afd60df6ca05
git-justinpodur <git-justin@podur.org>
2014-07-17 15:12:14 -0400

    added formatted goal, formatted level, formatted amount for cents to dollars

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,4 +1,9 @@
 class Pledge < ActiveRecord::Base
 	belongs_to :project
 	belongs_to :user
+
+		def formatted_amount
+			amount_in_dollars = amount_in_cents.to_f / 100
+			sprintf("%.2f", amount_in_dollars)
+		end	
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,3 +1,8 @@
 class Reward < ActiveRecord::Base
 	belongs_to :project
+
+		def formatted_level
+			level_in_dollars = level.to_f / 100
+			sprintf("%.2f", level_in_dollars)
+		end	
 end
