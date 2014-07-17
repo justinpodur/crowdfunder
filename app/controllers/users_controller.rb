63f6c8f8a8f68bd0752e0684d46e70bd76ab8809
Justine Jones <justinejones91@gmail.com>
2014-07-17 17:51:45 -0400

    the user edit form now looks fine

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-  	params.require(:user).permit(:email, :password, :password_confirmation, :name)
+  	params.require(:user).permit(:email, :password, :password_confirmation, :name, :bio)
   end
   
 end
