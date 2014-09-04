528a49a1e19654566081a4c1b462ca556fd95e58
Justine Jones <justinejones91@gmail.com>
2014-09-04 19:50:54 -0400

    moved has secure password to the top

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_secure_password
 	
 	has_many :pledges
 	has_many :backed_projects, :through => :pledges, source: :project
@@ -7,5 +8,4 @@ class User < ActiveRecord::Base
 	#reward per project
 
 	validates_presence_of :name, :email, :password
-	has_secure_password
 end
