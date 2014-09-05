e9c452b26cc619b3dfda4abe1500907c95c25188
Justine Jones <justinejones91@gmail.com>
2014-09-04 20:09:06 -0400

    added the image field to the user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-  	params.require(:user).permit(:email, :password, :password_confirmation, :name, :bio)
+  	params.require(:user).permit(:email, :password, :password_confirmation, :name, :bio, :image)
   end
   
 end
