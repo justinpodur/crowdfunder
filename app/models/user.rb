150a154b0409f015b230112f0037a2555de6e012
Justine Jones <justinejones91@gmail.com>
2014-09-05 10:30:56 -0400

    mounted the uploader for the image

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ActiveRecord::Base
 	has_many :owned_projects, foreign_key: "owner_id", class_name: "Project", dependent: :destroy
 	has_many :rewards, :through => :projects, dependent: :destroy #i think this means one 
 	#reward per project
-
+	mount_uploader :image, ImageUploader
 	validates_presence_of :name, :email, :password
 end
