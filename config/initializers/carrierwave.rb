dbec0da9de12c455479d3a32d99a708026feb778
Justine Jones <justinejones91@gmail.com>
2014-09-10 20:14:20 -0400

    fixed the carrierwave loop

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,10 @@
+CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV["aws_access_key_id"],                        # required
     :aws_secret_access_key  => ENV["aws_secret_access_key"],                        # required
     :region                 => 'us-west-2'
   }
-  config.fog_directory  = 'crowdfunderbucket'                     # required
+  config.fog_directory  = 'rain-forestbucket'                     # required
   config.fog_public     = true                                   # optional, defaults to true
 end
